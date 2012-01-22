6cb27430ad47e254fedd275b426f4559d9700203
Scott Burch <scott@toto.bulldoginfo.com>
2012-01-21 23:21:05 -0800

    converted desktop module to use mustache module

--- modules/desktop/desktop.js
+++ modules/desktop/desktop.js
@@ -1,10 +1,9 @@
 defineModule({name:'desktop', category:'task', description:'Desktop'}, function(that) {
 
     that.on_moduleManager_modulesLoaded = function() {
-        that.require(['text!desktop.html'], function(template){
-            that.doAction('setInnerHtml', {selector: 'body', html:template});
+        that.doAction('renderReplace', {selector:'body', template:'desktop.html',  cb: function() {
             that.fireEvent('ready');
-        });
+        }});
     };
 
 

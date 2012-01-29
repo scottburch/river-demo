706c0c2d8eb11e3dfdf8aab68cd137c40fdf06a8
Scott Burch <scott@toto.bulldoginfo.com>
2012-01-29 11:17:57 -0800

    moved mustache.js under the mustache module

--- modules/mustache/mustache.js
+++ modules/mustache/mustache.js
@@ -1,4 +1,4 @@
-defineModule({name:'moustache', category:'lib', description:'render moustache templates', requires:['vendor/mustache.js']}, function (that) {
+defineModule({name:'moustache', category:'lib', description:'render moustache templates', requires:['modules/mustache/vendor/mustache.js']}, function (that) {
 
     that.do_renderReplace = function (data, module) {
         module.require('text!'+data.template, function (template) {

c11e5f90d1db045e9e4f1cf3043184fc3ec5eccf
Scott Burch <scott@toto.bulldoginfo.com>
2012-03-22 05:28:39 -0700

    updated to take advantage of new river improvements

--- boot.js
+++ boot.js
@@ -2,14 +2,14 @@ define(function() {
     "use strict";
 
     window.requireConfig = {
-        paths: {river: 'vendor/river',modules:'modules'}
+        paths: {river: 'vendor/river'}
     };
     require.config(requireConfig);
 
 
 
     require(['river/lib/Application'], function(App) {
-        App.start('js', [
+        App.start('modules', [
             {path:'logging'},
             {path:'eventLogging'},
             {path:'commonUtils'},
--- modules/desktop/desktop.js
+++ modules/desktop/desktop.js
@@ -1,4 +1,4 @@
-defineModule({name:'desktop', category:'task', description:'Desktop'}, function(that) {
+defineModule(function(that) {
 
     that.on_moduleManager_modulesLoaded = function() {
         that.doAction('renderReplace', {selector:'body', template:'desktop.html',  cb: function() {

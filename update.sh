98cc5cf5a36223d65b65ada00937db460bf34f83
Scott Burch <scott@toto.bulldoginfo.com>
2012-03-26 15:19:33 -0700

    updating gh-pages

--- update.sh
+++ update.sh
@@ -1,14 +1,19 @@
+# remove old demo dir and redownload
 git rm -rf demo
 git clone git@github.com:scottburch/river-demo.git demo
 
-
+# update submodules
 cd demo
-
 git submodule init
 git submodule update 
 
+# remove any signs that this is a git repository
 find . -name .git | xargs rm -rf
+find . -name .gitignore | xargs rm -rf
+find . -name .gitmodules | xargs rm -rf
+
+# commit new site
 cd ..
 git add demo
-git commit -m 'updating gh-pages'
+git commit -a -m 'updating gh-pages'
 git push origin gh-pages

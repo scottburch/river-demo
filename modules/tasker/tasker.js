8652c428e713e9dbbff24a8ad71a5b41a85d6cb9
Scott Burch <scott@toto.bulldoginfo.com>
2012-03-27 19:00:41 -0700

    updated to use knockout ready event

--- modules/tasker/tasker.js
+++ modules/tasker/tasker.js
@@ -1,6 +1,6 @@
 defineModule(function(that) {
 
-    that.on_moduleManager_modulesLoaded = function() {
+    that.on_knockout_ready = function() {
         that.doAction('loadCss', {href:"css/tasker.css"});
         that.doAction('renderKoTemplate', {name:'tasker', to:'page', template:'templates/tasker.html', viewModel:{}});
     };

b331a1b6d761925d9e254f803d3addc96837e96f
Scott Burch <scott@bulldoginfo.com>
2012-04-03 15:28:37 -0700

    updating gh-pages

--- demo/modules/tasker/tasker.js
+++
@@ -1,7 +0,0 @@
-defineModule(function(that) {
-
-    that.on_knockout_ready = function() {
-        that.doAction('loadCss', {href:"css/tasker.css"});
-        that.doAction('renderKoTemplate', {name:'tasker', to:'page', template:'templates/tasker.html', viewModel:{}});
-    };
-});

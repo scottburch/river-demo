d596b3a7afd4f5c80273ef6f53726bcd8f1cb930
Scott Burch <scott@bulldoginfo.com>
2012-04-03 19:41:33 -0700

    updating gh-pages

--- update.sh
+++ update.sh
@@ -1,11 +1,21 @@
 # remove old demo dir and redownload
-git rm -rf demo
-git clone git@github.com:scottburch/river-demo.git demo
+rm -rf demo
+
+#git clone git@github.com:scottburch/river-demo.git demo
+git clone ../river-demo demo
 
 # update submodules
 cd demo
 git submodule init
 git submodule update 
+
+# add in the modules that are loaded remotely
+for mod in loaders logging eventLogging knockout jasmine
+do
+git clone git@github.com:scottburch/river-$mod.git modules/$mod
+done 
+
+
 
 # remove any signs that this is a git repository
 find . -name .git | xargs rm -rf

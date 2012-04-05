53c258b1e386bbf595b31a6635f76e9bf92f7cd1
Scott Burch <scott@bulldoginfo.com>
2012-04-04 21:16:08 -0700

    externalized Task to separate file in domain

--- modules/domain/domain.js
+++ modules/domain/domain.js
@@ -1,35 +1,24 @@
-defineModule(function(that) {
+defineModule(function (that) {
 
     var tasks = {};
 
-    that.on_serverRequest_taskReceived = function(rawTask) {
+    that.on_serverRequest_taskReceived = function (rawTask) {
         var task = tasks[rawTask.id];
-        if(task) {
+        if (task) {
             task.update(rawTask);
             that.fireEvent('taskUpdated', task);
         } else {
-            task = tasks[rawTask.id] = Task(rawTask);
-            that.fireEvent('taskAdded', task);
+            that.require('Task', function (Task) {
+                task = tasks[rawTask.id] = Task(rawTask);
+                that.fireEvent('taskAdded', task);
+            });
         }
     };
 
-    that.on_serverRequest_taskDeleted = function(data) {
+    that.on_serverRequest_taskDeleted = function (data) {
         tasks[data.id] = undefined;
-        that.fireEvent('taskDeleted',{id:data.id});
+        that.fireEvent('taskDeleted', {id:data.id});
     };
-
-    function Task(rawTask) {
-        var that = {};
-        that.update = function(rawTask) {
-            that.id = rawTask.id;
-            that.title = rawTask.title;
-            that.description = rawTask.description;
-        };
-
-        that.update(rawTask);
-        return that;
-    };
-
 
 
 });
